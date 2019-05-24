1bb3bdf8f642b153b0fa89ed48b2f9571c9aa46d
Alberto Bastos <albertobastos@gmail.com>
2019-05-24 18:15:32 +0200

    Use event-stream instead of readline

--- src/part3_worker.js
+++ src/part3_worker.js
@@ -1,19 +1,19 @@
 const cluster = require('cluster');
-const readline = require('readline');
 const fs = require('fs');
+const es = require('event-stream');
 
 if (cluster.isMaster) {
-    console.error('This module cannot be required at master process.');
-    process.exit(-1);
+  console.error('This module cannot be required at master process.');
+  process.exit(-1);
 }
 
 process.on('message', function ({ filename, filepath, terms }) {
-    getFileStats(filepath, terms).then((frequencies) => {
-        process.send({
-            filename,
-            frequencies
-        });
+  getFileStats(filepath, terms).then((frequencies) => {
+    process.send({
+      filename,
+      frequencies
     });
+  });
 });
 
 /**
@@ -22,31 +22,31 @@ process.on('message', function ({ filename, filepath, terms }) {
  * and the total word count using the special key _.
  */
 function getFileStats(filepath, terms) {
-    return new Promise((resolve, reject) => {
-        // result[term] = number of occurences of the term
-        // result._ = total word count
-        const result = terms.reduce((acc, term) => {
-            acc[term] = 0;
-            return acc;
-        }, { _: 0 });
+  return new Promise((resolve, reject) => {
+    // result[term] = number of occurences of the term
+    // result._ = total word count
+    const result = terms.reduce((acc, term) => {
+      acc[term] = 0;
+      return acc;
+    }, { _: 0 });
 
-        const rl = readline.createInterface(fs.createReadStream(filepath), null);
-
-        rl.on('line', line => {
-            // update total word count (ignoring extra white spaces)
-            result._ += line.split(' ').filter(x => !!x).length;
-            // update count for each term
-            terms.forEach(term => {
-                const match = line.match(new RegExp(`\\b${term}\\b`, 'ig'));
-                match && (result[term] += match.length);
-            });
-        });
-
-        rl.on('close', () => {
-            // return file stats
-            resolve(result);
-        });
-
-        rl.on('error', reject);
-    });
+    fs
+      .createReadStream(filepath)
+      .pipe(es.split())
+      .pipe(es
+        .mapSync((line) => {
+          // update total word count (ignoring extra white spaces)
+          result._ += line.split(' ').filter(x => !!x).length;
+          // update count for each term
+          terms.forEach(term => {
+            const match = line.match(new RegExp(`\\b${term}\\b`, 'ig'));
+            match && (result[term] += match.length);
+          });
+        })
+        .on('end', () => {
+          resolve(result);
+        })
+        .on('error', reject)
+      );
+  });
 }
